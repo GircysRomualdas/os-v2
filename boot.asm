952e50f5fc799a52706757e0699e46ecad1d97a7
Romas <r2udis@gmail.com>
2026-04-18 01:19:16 +0300

    Add boot

--- boot.asm
+++ boot.asm
@@ -1,3 +1,25 @@
 BITS 32
 
+section .text
+  align 4
+  dd 0x1badb002
+  dd 0x00000000
+  dd -(0x1badb002 + 0x00000000)
 
+global start
+extern kmain
+
+start:
+  cli
+  mov esp, stack_space
+  call kmain
+  hlt
+
+halt:
+  cli
+  hlt
+  jmp halt
+
+section .bss
+RESB 8192
+stack_space:
